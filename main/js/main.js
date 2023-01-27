3b2598a6c1c25628322bf00b1952c7a7c54a2ba6
Vladislav <dd.Anghold@yandex.ru>
2023-01-28 00:07:41 +0800

    1

--- main/js/main.js
+++ main/js/main.js
@@ -1,11 +1,11 @@
 document.getElementById("select")
-.onchange = function () {
-    var b = {
-        1: "vanila",
-        2: "railcraft",
-        3: "lotr"
-    }, c = this.value,
-        a;
-    for (a in b) document.getElementById(b[a])
-        .style.display = 0 == c || c == a ? "block" : "none"
+    .onchange = function () {
+        var b = {
+            1: "vanila",
+            2: "railcraft",
+            3: "lotr"
+        }, c = this.value,
+            a;
+        for (a in b) document.getElementById(b[a])
+            .style.display = 0 == c || c == a ? "block" : "none"
 };
