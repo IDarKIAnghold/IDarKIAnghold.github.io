13f15b7260f5c3900bfba56fe1fb952bce975ccd
Vladislav <dd.Anghold@yandex.ru>
2023-02-08 10:48:59 +0800

    1

--- main/js/select.js
+++ main/js/select.js
@@ -1,21 +1,21 @@
-document.getElementById("select")
-.onchange = () => {
-    var b = {
-        1: "vanila",
-        2: "railcraft",
-    }, c = this.value,
-        a;
-    for (a in b) document.getElementById(b[a])
-    .style.display = 0 == c || c == a ? "block" : "none"
-    
-    document.getElementById("input").value = "";
-    let item = document.querySelectorAll('.crafting-app__items .crafting-app__item');
-    item.forEach(elem => elem.classList.remove('hide'))
-    let mode = document.querySelector('.crafting-app__recipe');
-    if(c == 1){
-        mode.classList.replace("railcraft" , "vanila");
-    }
-    else{
-        mode.classList.replace("vanila" , "railcraft");
-    }
-};
+    document.getElementById("select")
+    .onchange = () => {
+        var b = {
+            1: "vanila",
+            2: "railcraft",
+        }, c = this.value,
+            a;
+        for (a in b) document.getElementById(b[a])
+        .style.display = 0 == c || c == a ? "block" : "none"
+        
+        document.getElementById("input").value = "";
+        let item = document.querySelectorAll('.crafting-app__items .crafting-app__item');
+        item.forEach(elem => elem.classList.remove('hide'))
+        let mode = document.querySelector('.crafting-app__recipe');
+        if(c == 1){
+            mode.classList.replace("railcraft" , "vanila");
+        }
+        else{
+            mode.classList.replace("vanila" , "railcraft");
+        }
+    };
