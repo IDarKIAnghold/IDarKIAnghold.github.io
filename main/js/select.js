c5141f87c218490518e4b153e4c0a5b0289f0eb3
Vladislav <dd.Anghold@yandex.ru>
2023-02-12 02:53:20 +0800

    Update select.js

--- main/js/select.js
+++ main/js/select.js
@@ -5,17 +5,20 @@ var ddData = [
 
 import crafting_table_V from "./craft_dbv.js";
 
-var vanila = "<div class='scrollbar vanila' id='all-items'>"
-var railcraft = "<div class='scrollbar railcraft'id='all-items'>"
+var vanila = "<div class='scrollbar vanila' id='item-list'>"
+var railcraft = "<div class='scrollbar railcraft'id='item-list'>"
+$('.crafting-app__recipe').addClass('vanila');
 $('#select').ddslick({
     data:ddData,
     width: 120,
     onSelected: function (e) {
         let c = e.selectedData.value;
         if(c==1){
-            $('.crafting-app__items').empty().append(vanila).append(crafting_table_V)
+            $('.crafting-app__items').empty().append(vanila).append(crafting_table_V);
+            $('.railcraft').toggleClass("vanila railcraft");
         }else{ 
-            $('.crafting-app__items').empty().append(railcraft).append()
+            $('.crafting-app__items').empty().append(railcraft).append();
+            $('.vanila').toggleClass("vanila railcraft");
         }
         document.getElementById("input").value = ""; 
         $('.category_active').removeClass('category_active')
