e65e906e68d8772158e055c959b322882f101741
Vladislav <dd.Anghold@yandex.ru>
2023-02-12 13:31:16 +0800

    1

--- main/js/categories.js
+++ main/js/categories.js
@@ -1,39 +1,41 @@
-let categories = {
-    1:{ id: 0, name: "Все" }, 
-    2:{ id: 7, name: "Еда" }, 
-    3:{ id: 8, name: "Инструменты" }, 
-    4:{ id: 9, name: "Оружие" }, 
-    5:{ id: 11, name: "Строительные блоки" }, 
-    6:{ id: 1, name: "Декоративные блоки" },
-    7:{ id: 2, name: "Механизмы" }, 
-    8:{ id: 3, name: "Транспорт" }, 
-    9:{ id: 6, name: "Разное" }
-};
-
-const categoryBody = document.getElementById('craffting-app__categories'),
-category_active = categoryBody.getElementsByClassName('crafting-app__category')
-
-Object.keys(categories).forEach(function(key) {
-  const category = document.createElement('div')
-  category.dataset.filter = categories[key].id
-  category.textContent = categories[key].name
-  if(categories[key].id == 0) 
-    category.className = 'crafting-app__category category_active crafting-app__All'
-  else
-    category.className = 'crafting-app__category'
-  category.onclick = function(){
-    input.value = ''
-    for (let i = 0,length = category_active.length; i < length; i++) {
-      let  current = document.getElementsByClassName("category_active")
-      current[0].className = current[0].className.replace(" category_active", "")
-      this.className += " category_active"
-    }
-    document.querySelectorAll('.crafting-app__item').forEach((c) =>{
-      if (c.getAttribute('data-tab') == categories[key].id || categories[key].id == 0)
-        $(c).removeClass('hide');
-      else
-        $(c).addClass('hide');
-    })
+let categories 
+(function getFile (categories) {
+  let request = new XMLHttpRequest();  
+  request.open('GET', categories);
+  request.onloadend = function() {
+      parse(request.responseText);
   }
-  categoryBody.appendChild(category)
-})
+  request.send();
+})('./json/categories.json')
+
+function parse(obj){
+  categories = JSON.parse(obj)
+  
+  const categoryBody = document.getElementById('craffting-app__categories'),
+  category_active = categoryBody.getElementsByClassName('crafting-app__category')
+
+  Object.keys(categories).forEach(function(key) {
+    const category = document.createElement('div')
+    category.dataset.filter = categories[key].id
+    category.textContent = categories[key].name
+    if(categories[key].id == 0) 
+      category.className = 'crafting-app__category category_active crafting-app__All'
+    else
+      category.className = 'crafting-app__category'
+    category.onclick = function(){
+      input.value = ''
+      for (let i = 0,length = category_active.length; i < length; i++) {
+        let  current = document.getElementsByClassName("category_active")
+        current[0].className = current[0].className.replace(" category_active", "")
+        this.className += " category_active"
+      }
+      document.querySelectorAll('.crafting-app__item').forEach((c) =>{
+        if (c.getAttribute('data-tab') == categories[key].id || categories[key].id == 0)
+          $(c).removeClass('hide');
+        else
+          $(c).addClass('hide');
+      })
+    }
+    categoryBody.appendChild(category)
+  })
+}
