99dd337b29ddda8fea6b6227f20f92c4d06b1a6e
Vladislav <dd.Anghold@yandex.ru>
2023-02-14 18:47:59 +0800

    1

--- main/js/craft_dbv.js
+++ main/js/craft_dbv.js
@@ -3,7 +3,9 @@ export default () =>{
     const ElemBody = document.getElementById('item-list'),
     setItemTitle = document.getElementById('crafting-app__title'),
     recipe_output = document.querySelector('.recipe_output i'),
-    recipe_index = document.querySelector('.recipe_control-index');
+    recipe_index = document.querySelector('.recipe_control-index'),
+    next_btn = document.querySelector('recipe_control-btn_next'),
+    prev_btn = document.querySelector('recipe_control-btn_prev');
 
     (function getFile (crafting_dbv) {
         let request = new XMLHttpRequest();  
@@ -25,8 +27,9 @@ export default () =>{
             else
                 iElem.dataset.tab = crafting_dbv[key].tab
             iElem.onclick = () =>{
+                let i = 1
                 setItemTitle.textContent = "Крафт предмета " + crafting_dbv[key].name;
-                recipe_index.textContent = '1/' + crafting_dbv[key].recipes.length
+                recipe_index.textContent = i + '/' + crafting_dbv[key].recipes.length
                 recipe_output.className = (`v${crafting_dbv[key].id}`)
                 recipe_output.title = crafting_dbv[key].name
             }  
