02cb2215a485faadec9dba8ff327ab981989d6f0
Vladislav <dd.Anghold@yandex.ru>
2023-02-14 22:47:41 +0800

    Update craft_dbv.js

--- main/js/craft_dbv.js
+++ main/js/craft_dbv.js
@@ -5,7 +5,8 @@ export default () =>{
     recipe_output = document.querySelector('.recipe_output i'),
     recipe_index = document.querySelector('.recipe_control-index'),
     next_btn = document.querySelector('.recipe_control-btn_next'),
-    prev_btn = document.querySelector('.recipe_control-btn_prev');
+    prev_btn = document.querySelector('.recipe_control-btn_prev'),
+    recipeType = document.querySelector('.recipe');
 
     (function getFile (crafting_dbv) {
         let request = new XMLHttpRequest();  
@@ -31,11 +32,12 @@ export default () =>{
             iElem.onclick = () =>{
                 setItemTitle.textContent = "Крафт предмета " + crafting_dbv[key].name;
                 recipe_index.textContent = counter + '/' + crafting_dbv[key].recipes.length;
+                recipeType.className = `recipe recipe_${crafting_dbv[key].recipes[index].type}`
                 next_btn.onclick = () =>{
                     if(counter < crafting_dbv[key].recipes.length){
                         counter++;
                         index++;
-                        console.log(crafting_dbv[key].recipes[index].type);
+                        recipeType.className = `recipe recipe_${crafting_dbv[key].recipes[index].type}`
                         recipe_index.textContent = counter + '/' + crafting_dbv[key].recipes.length
                     }
                 } 
@@ -43,7 +45,7 @@ export default () =>{
                     if(counter <= crafting_dbv[key].recipes.length & counter > 1){
                         counter--;
                         index--;
-                        console.log(crafting_dbv[key].recipes[index].type);
+                        recipeType.className = `recipe recipe_${crafting_dbv[key].recipes[index].type}`
                         recipe_index.textContent = counter + '/' + crafting_dbv[key].recipes.length
                     }
                 }
