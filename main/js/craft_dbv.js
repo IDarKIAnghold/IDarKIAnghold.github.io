a734390a7cd5f2ecb1834147603dc923de7409a2
Vladislav <dd.Anghold@yandex.ru>
2023-02-14 22:32:07 +0800

    Update craft_dbv.js

--- main/js/craft_dbv.js
+++ main/js/craft_dbv.js
@@ -20,7 +20,7 @@ export default () =>{
         crafting_dbv = JSON.parse(obj);
         Object.keys(crafting_dbv).forEach(function(key) {
             const iElem = document.createElement('i');
-            let i = 1;
+            let counter = 1;
             let index = 0
             iElem.className = `crafting-app__item v${crafting_dbv[key].id}`;
             iElem.title = crafting_dbv[key].name;
@@ -30,21 +30,21 @@ export default () =>{
                 iElem.dataset.tab = crafting_dbv[key].tab
             iElem.onclick = () =>{
                 setItemTitle.textContent = "Крафт предмета " + crafting_dbv[key].name;
-                recipe_index.textContent = i + '/' + crafting_dbv[key].recipes.length;
+                recipe_index.textContent = counter + '/' + crafting_dbv[key].recipes.length;
                 next_btn.onclick = () =>{
-                    if(i < crafting_dbv[key].recipes.length){
-                        i++;
+                    if(counter < crafting_dbv[key].recipes.length){
+                        counter++;
                         index++;
                         console.log(crafting_dbv[key].recipes[index].type);
-                        recipe_index.textContent = i + '/' + crafting_dbv[key].recipes.length
+                        recipe_index.textContent = counter + '/' + crafting_dbv[key].recipes.length
                     }
                 } 
                 prev_btn.onclick = () =>{
-                    if(i <= crafting_dbv[key].recipes.length & i > 1){
-                        i--;
+                    if(counter <= crafting_dbv[key].recipes.length & counter > 1){
+                        counter--;
                         index--;
                         console.log(crafting_dbv[key].recipes[index].type);
-                        recipe_index.textContent = i + '/' + crafting_dbv[key].recipes.length
+                        recipe_index.textContent = counter + '/' + crafting_dbv[key].recipes.length
                     }
                 }
                 recipe_output.className = (`v${crafting_dbv[key].id}`)
